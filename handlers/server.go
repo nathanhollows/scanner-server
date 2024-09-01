56105ff25b7f9c200151c9e7386bb0d3c0f28734
nathanhollows <me@nathanhollows.com>
2024-09-01 14:32:48 +1200

    chore: remove dead endpoints + db init

--- handlers/server.go
+++ handlers/server.go
@@ -16,30 +16,8 @@ func Start() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexAction)
-	mux.HandleFunc("/register", registerAction)
 	mux.HandleFunc("/scan", scanAction)
 	mux.HandleFunc("/generate", generateHandler)
-
-	var err error
-	db, err = sql.Open("sqlite3", dbname)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS scans (location_id TEXT, tag TEXT KEY, timestamp TEXT KEY)")
-	if err != nil {
-		panic(err)
-	}
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS nodes (node TEXT PRIMARY KEY, location_id TEXT KEY)")
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS locations 
-		(id INTEGER PRIMARY KEY, name TEXT KEY, description TEXT KEY, link TEXT KEY)`)
-	if err != nil {
-		panic(err)
-	}
 
 	log.Fatal(http.ListenAndServe(":8088", mux))
 }
